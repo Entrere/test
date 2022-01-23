99c95fd98e34603e7f7f2683143ee585ec176b4e
Entrere <entrere@yandex.ru>
2022-01-24 00:20:34 +0300

    with image

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ let greeting = document.createElement('h1');
 greeting.textContent='Привет это подключенный файл Джава Скрипт!'
 let container = document.querySelector('.container');
 container.appendChild(greeting);
+let num = 100 * 15 + 34; 
+alert(num);

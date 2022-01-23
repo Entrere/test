def7cecd965db41e30f31df7f9ba6992960f22fc
Konstantin <m1llenium@yandex.ru>
2022-01-23 20:21:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let greeting = document.createElement('h1');
+greeting.textContent='Привет это подключенный файл Джава Скрипт!'
+let container = document.querySelector('.container');
+container.appendChild(greeting);
